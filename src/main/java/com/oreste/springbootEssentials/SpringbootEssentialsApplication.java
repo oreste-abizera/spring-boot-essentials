795e92eb277f090ed28d464b498ed647eb8996aa
oreste-abizera <oresteabizera11@gmail.com>
2021-12-31 19:39:25 +0200

    Initial commit

---
+++ src/main/java/com/oreste/springbootEssentials/SpringbootEssentialsApplication.java
@@ -0,0 +1,13 @@
+package com.oreste.springbootEssentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEssentialsApplication.class, args);
+	}
+
+}
