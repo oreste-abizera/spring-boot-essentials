b2c6eae17c791ab2dca7528a39d13c6af07f4cbf
oreste-abizera <oresteabizera11@gmail.com>
2022-01-01 19:43:12 +0200

    simple api

--- src/main/java/com/oreste/springbootEssentials/SpringbootEssentialsApplication.java
+++ src/main/java/com/oreste/springbootEssentials/SpringbootEssentialsApplication.java
@@ -2,12 +2,22 @@ package com.oreste.springbootEssentials;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class SpringbootEssentialsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootEssentialsApplication.class, args);
+	}
+
+	@GetMapping
+	public List<String> hello() {
+		return List.of("Hello", "World");
 	}
 
 }
