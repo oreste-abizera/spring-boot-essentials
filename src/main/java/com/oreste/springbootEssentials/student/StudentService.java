3973e4ee75104eb29e673929b96be1ebcdded966
oreste-abizera <oresteabizera11@gmail.com>
2022-01-04 23:16:30 +0200

    deleting students

--- src/main/java/com/oreste/springbootEssentials/student/StudentService.java
+++ src/main/java/com/oreste/springbootEssentials/student/StudentService.java
@@ -25,4 +25,13 @@ public class StudentService {
         studentRepository.save(student);
     }
 
+    public void deleteStudent(Long id){
+        boolean exists = studentRepository.existsById(id);
+        if(!exists){
+            throw new IllegalStateException("student with id " + id + " does not exist");
+        }
+
+        studentRepository.deleteById(id);
+    }
+
 }
