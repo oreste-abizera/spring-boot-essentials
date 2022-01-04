d810812316c9268a76643c11dbf4f94a4a088b3d
oreste-abizera <oresteabizera11@gmail.com>
2022-01-04 22:57:25 +0200

    POST mapping

--- src/main/java/com/oreste/springbootEssentials/student/StudentService.java
+++ src/main/java/com/oreste/springbootEssentials/student/StudentService.java
@@ -15,4 +15,8 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
+    public void addNewStudent(Student student){
+        System.out.println(student);
+    }
+
 }
