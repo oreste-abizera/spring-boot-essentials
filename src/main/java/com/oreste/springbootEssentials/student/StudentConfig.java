a0616ec264f00780fdebd777277c5a2ad6a70263
oreste-abizera <oresteabizera11@gmail.com>
2022-01-04 20:24:54 +0200

    JPA repository

---
+++ src/main/java/com/oreste/springbootEssentials/student/StudentConfig.java
@@ -0,0 +1,24 @@
+package com.oreste.springbootEssentials.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student mariam = new Student("Mariam","mariam@gmail.com", LocalDate.of(2000, Month.JANUARY,5),22);
+            Student alex = new Student("Alex","alex@gmail.com", LocalDate.of(2004, Month.JANUARY,5),18);
+
+            studentRepository.saveAll(List.of(mariam,alex));
+
+        };
+    }
+}
--- src/main/java/com/oreste/springbootEssentials/student/StudentService.java
+++ src/main/java/com/oreste/springbootEssentials/student/StudentService.java
@@ -1,15 +1,18 @@
 package com.oreste.springbootEssentials.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
     public List<Student> getStudents() {
-        return List.of(new Student(1L,"Mariam","mariam@gmail.com", LocalDate.of(2000, Month.JANUARY,5),21));
+        return studentRepository.findAll();
     }
 
 }
