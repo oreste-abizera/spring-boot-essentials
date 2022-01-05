0801b2c255124f239d3b18ea66a462ec494f6c95
oreste-abizera <oresteabizera11@gmail.com>
2022-01-05 02:16:24 +0200

    update students using transactions

--- src/main/java/com/oreste/springbootEssentials/student/StudentService.java
+++ src/main/java/com/oreste/springbootEssentials/student/StudentService.java
@@ -3,7 +3,9 @@ package com.oreste.springbootEssentials.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -32,6 +34,24 @@ public class StudentService {
         }
 
         studentRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email){
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(()->new IllegalStateException("student with id " + studentId + " does not exist"));
+
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+
+        if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(),email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()){
+                throw new IllegalStateException("Email is taken");
+            }
+            student.setEmail(email);
+        }
     }
 
 }
